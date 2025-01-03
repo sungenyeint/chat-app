3319b58a9eb1971b73a0a7964fe7542a8ba7eded
meepont <sungenyeint@gmail.com>
2025-01-03 21:02:26 +0630

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+let express = require('express');
+let socket = require('socket.io');
+
+let app = express();
+
+let server = app.listen(4000, () => {
+  console.log('Server is running on port 4000')
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+})
+
+let io = socket(server);
+io.on("connection", (socket) => {
+    console.log("socket conection is ok" + socket.id);
+    socket.on("chat", (data) => {
+        io.sockets.emit("chat", data)
+    })
+    socket.on("typing", (name) => {
+        console.log(name + " is typing...")
+        socket.broadcast.emit("typing", name);
+    })
+});
+
