4f6f422898c21cdc98e224ecde91566e540ecb80
meepont <sungenyeint@gmail.com>
2025-01-04 18:26:25 +0630

    add http and cors

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 let express = require('express');
+let http = require('http');
 let socket = require('socket.io');
+let cors = require('cors');
 
-let app = express();
+// let app = express();
 
-let server = app.listen(3000, () => {
-  console.log('Server is running on port 3000')
+// let server = app.listen(3000, () => {
+//   console.log('Server is running on port 3000')
+// });
+
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/public/index.html')
+// })
+// let io = socket(server);
+
+const app = express();
+const server = http.createServer(app);
+
+const io = socket(server, {
+    cors: {
+        origin: "https://chat-app-ten-pied.vercel.app", // Allow your Vercel client
+        methods: ["GET", "POST"],
+    },
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html')
-})
+// const PORT = process.env.PORT || 3000;
+server.listen(3000, () => {
+    console.log(`Server running on port 3000`);
+});
 
-let io = socket(server);
+
 io.on("connection", (socket) => {
     console.log("socket conection is ok" + socket.id);
     socket.on("chat", (data) => {
