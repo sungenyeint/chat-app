5d386d5128a95d555e92b7cb472acafc3d6ebcc0
meepont <sungenyeint@gmail.com>
2025-01-04 19:30:38 +0630

    fix NG

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ const io = socket(server, {
 });
 
 // const PORT = process.env.PORT || 3000;
-server.listen(3000, () => {
-    console.log(`Server running on port 3000`);
-});
+// server.listen(3000, () => {
+//     console.log(`Server running on port 3000`);
+// });
 
 
 io.on("connection", (socket) => {
