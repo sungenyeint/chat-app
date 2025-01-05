5750d7ef1d52cdcf88b3d90495491762c9af59ab
meepont <sungenyeint@gmail.com>
2025-01-05 10:35:54 +0630

    add vercel.json

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
-let express = require('express');
-let socket = require('socket.io');
+const express = require('express');
+const { Server } = require('socket.io');
+const path = require('path');
 
-let app = express();
+const app = express();
+const port = process.env.PORT || 4000;
 
-let server = app.listen(3000, () => {
-  console.log('Server is running on port 3000')
-});
+// Serve static files from the public directory
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html')
-})
-
-let io = socket(server);
-io.on("connection", (socket) => {
-    console.log("socket conection is ok" + socket.id);
-    socket.on("chat", (data) => {
-        io.sockets.emit("chat", data)
-    })
-    socket.on("typing", (name) => {
-        console.log(name + " is typing...")
-        socket.broadcast.emit("typing", name);
-    })
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+const server = app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log("socket connection is ok " + socket.id);
+
+    socket.on("chat", (data) => {
+        io.sockets.emit("chat", data);
+    });
+
+    socket.on("typing", (name) => {
+        console.log(name + " is typing...");
+        socket.broadcast.emit("typing", name);
+    });
+});
